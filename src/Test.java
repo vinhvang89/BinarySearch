a20644e0c8a9879411d197e40c288dcebac43da7
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-04-19 14:44:27 +0700

    new

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(BinarySearch.binarySearch(BinarySearch.list,2));
+        System.out.println(BinarySearch.binarySearch(BinarySearch.list,44));
+        System.out.println(BinarySearch.binarySearch(BinarySearch.list,72));
+        System.out.println(BinarySearch.binarySearch(BinarySearch.list,79));
+
+    }
+}
